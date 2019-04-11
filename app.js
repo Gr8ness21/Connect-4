69613564917ea9c515fe8844e24091e66d6e9533
Gr8ness21 <pmleechapman@gmail.com>
2019-04-11 10:42:34 -0400

    created a table of collumns and rows

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 // *Comments for structure and organization
+// Game consists of players, two "tokens", rows and columns. I need to...
 
-// Game consists of players, two "tokens", rows and columns.
+// 1. Establish variables and Set up a 6 row - 7 collumn board
+// 2. Fill the board with empty characters
+// 3. Create a game loop
+// 4. Show Board
+// 5. Ask Player for move then try
+// 6. If it works -> check for winner -> if there is a winner end game, record score, switch players.
