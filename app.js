a61d463a5752cd372cb37b8bd4485e51ac44ffef
Gr8ness21 <pmleechapman@gmail.com>
2019-04-14 23:22:57 -0400

    researched different approach to logic with through w3 and mdn. Creating New Game

--- app.js
+++ app.js
@@ -6,52 +6,50 @@
 // 3. Create a game loop end of game will signal start of a new one
 // 5. Ask Player for move then try
 // 6. If it works -> check for winner -> if there is a winner end game, record score, switch players.
-let $board = [];
-const playerToken = 
 
 $(document).ready(function(){
-// push functions into my established grid. Using "this" will localize commands. Makes jquery more easily readable.
+// // push functions into my established grid. Using "this" will localize commands. Makes jquery more easily readable.
 
-//Build board
-const $board = [
-    [{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{}],
-    ];
-
-
-//  render board
-const render = () =>{
-    const $board = document.getElementById('connect4');
-    // let doc = ``;
-}
-render();
+// //Build board
+// const $board = [
+//     [{},{},{},{},{},{},{}],
+//     [{},{},{},{},{},{},{}],
+//     [{},{},{},{},{},{},{}],
+//     [{},{},{},{},{},{},{}],
+//     [{},{},{},{},{},{},{}],
+//     [{},{},{},{},{},{},{}],
+//     ];
 
 
-//Make columns clickable
-$('.column').click(function(){
-    $this = $(this);
-    $this.html(playerToken);
-    const c = $this.data('c');
-    const r = $this.data('r');
-    $board[r][c] = playerToken;
-    console.log($board);
-});
+// //  render board
+// const render = () =>{
+//     const $board = document.getElementById('connect4');
+//     // let doc = ``;
+// }
+// render();
 
-function findLastEmptyCell(){
-    const spots = $(`.column[data-c='${column}']`);
-    for(let r = spots.length - 1; r>=0; r++){
-        const $spots = $(spots[r]);
-        if($spots.hasClass('empty')){
-            return $spots;
-            console.log("working")
-        }
-    }
-    return null
-};
+
+// //Make columns clickable
+// $('.column').click(function(){
+//     $this = $(this);
+//     $this.html(playerToken);
+//     const c = $this.data('c');
+//     const r = $this.data('r');
+//     $board[r][c] = playerToken;
+//     console.log($board);
+// });
+
+// function findLastEmptyCell(){
+//     const spots = $(`.column[data-c='${column}']`);
+//     for(let r = spots.length - 1; r>=0; r++){
+//         const $spots = $(spots[r]);
+//         if($spots.hasClass('empty')){
+//             return $spots;
+//             console.log("working")
+//         }
+//     }
+//     return null
+// };
 
 // $board.on('mouseenter', 'column.empty', function(){
 //     const column = $(this).data('column');
@@ -64,7 +62,7 @@ function findLastEmptyCell(){
 
 
 
-
+//Attempting to approach using jquery to create table and manipulate DOM
 
 
 
